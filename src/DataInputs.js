6181b70e0cc03a84e88c9d7c6b3ec4d1e4371ee4
Adnan Ali <adnanali496@gmail.com>
2022-12-04 14:16:39 +0530

    commit

--- src/DataInputs.js
+++ src/DataInputs.js
@@ -1,34 +1,6 @@
 import React, { useState } from 'react'
 
 function DataInputs() {
-    const handleInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        console.log (name, value);
-        setUserDetails( {...userDetails, [name]:value}
-            )
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
-
-    const newRecords = {...userDetails, id: new Date().getTime().toString()
-        
-    };
-    setUserDetails({
-        username: "",
-        email:"",
-        phone:"",
-        password:""
-    })
-    console.log(records)
-    setRecords ([...records, newRecords]);
-   
-
-
-    const [records, setRecords] = useState([])
-
     const [userDetails, setUserDetails] = useState({
         username: "",
         email: "",
@@ -38,6 +10,48 @@ function DataInputs() {
     }
     );
 
+    const [records, setRecords] = useState([]);
+
+
+    const handleInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        console.log (name, value);
+
+        setUserDetails( {...userDetails, [name] :value})
+};
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newRecord = { ...userDetails, id : new Date().getTime().toString() }
+
+            setRecords([...records, newRecord]);
+            console.log(records)
+
+            setUserDetails({
+                username: "",
+                email:"",
+                phone:"",
+                password:""
+            })
+        
+        };
+    
+
+   
+    
+   
+    // setRecords ([...records, newRecords]);
+   
+
+
+
+
+    
+  
+
   return (
     <>
 
@@ -45,33 +59,33 @@ function DataInputs() {
             <div>
             <label htmlFor='username' > Full Name :</label>
             <input  type="text" autoComplete='off' 
-                    name='username' id='username'
                     value={userDetails.name} onChange={handleInput} 
+                    name='username' id='username'
                     className='mx-3'></input>
              </div>
             
             <div>
-            <label htmlFor='username' > E-mail :</label>
+            <label htmlFor='email' > E-mail :</label>
             <input  type="text" autoComplete='off'  
-                    name='email' id='username'
                     value={userDetails.email} onChange={handleInput} 
+                    name='email' id='email'
                     className='mx-3'>
              </input>
             </div>
 
             <div>
-            <label htmlFor='username' > Contact : </label>
-            <input  type="text" autoComplete='off' 
-                    name='phone' id='username'  
+            <label htmlFor='phone' > Contact : </label>
+            <input  type="text" autoComplete='off'
                     value={userDetails.phone} onChange={handleInput} 
+                    name='phone' id='phone'  
                     className='mx-3'></input>
             </div>
 
             <div>
-            <label htmlFor='username' > Password :</label>
+            <label htmlFor='password' > Password :</label>
             <input  type="password" autoComplete='off'
-                    name='password' id='username' 
                     value={userDetails.password} onChange={handleInput} 
+                    name='password' id='password' 
                     className='mx-3'></input>
             </div>
             
@@ -99,6 +113,6 @@ function DataInputs() {
         
     
   )
-}
 
+        }
 export default DataInputs
